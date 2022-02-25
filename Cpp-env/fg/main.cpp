037ebd6c9a9d53ea2755144b7585bd55e8d24534
willcmc <w.ebenezer007@gmail.com>
2022-02-25 11:00:20 +0530

    generic implemented

--- Cpp-env/fg/main.cpp
+++ Cpp-env/fg/main.cpp
@@ -1,35 +1,53 @@
-#include <sys/types.h>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <netdb.h>
-#include <memory.h>
-#include <ifaddrs.h>
-#include <net/if.h>
-#include <errno.h>
-#include <stdlib.h>
-#include <iostream>
-#include <sstream>
+#include "main.h"
 #include "UDPConnect.h"
 
 
-void launch_fg(const std::string& aircraft, const std::string& rate, const std::string& server, const std::string& port, const std::string& protocol){
+void launch_fg(const std::string& aircraft, const std::string& rate, 
+                const std::string& server, const std::string& port, 
+                const std::string& protocol){
+    // Start command stringstream
+    std::cout << "Starting FlightGear.. please wait\n";
     std::string run_fg;
     run_fg = "fgfs";
     std::stringstream cmd(run_fg);
 
+    // Check if already running
+    if (strlen(exec("ps -h | grep '[f]gfs'").c_str())>0){
+        std::cout << "Already running.\n";
+        return;
+    }
+
     // constructing the command
+    // UDP - Native
+    // cmd << run_fg;
+    // cmd << " --fg-aircraft=/home/willcmc/.fgfs/Aircraft/f18 ";
+    // cmd << " --aircraft=";
+    // cmd << aircraft;
+    // cmd << " --native-fdm=";
+    // cmd << "socket,";
+    // cmd << "in,";
+    // cmd << rate << ",";
+    // cmd << server << ",";
+    // cmd << port << ",";
+    // cmd << protocol;
+    // cmd << " --fdm=null";
+    // cmd << " --disable-ai-traffic";
+    // cmd << " --disable-real-weather-fetch";
+    // cmd << " --timeofday=morning";
+    // cmd << " --disable-sound &";
+
+    // UDP - Generic
     cmd << run_fg;
     cmd << " --fg-aircraft=/home/willcmc/.fgfs/Aircraft/f18 ";
     cmd << " --aircraft=";
     cmd << aircraft;
-    cmd << " --native-fdm=";
+    cmd << " --generic=";
     cmd << "socket,";
     cmd << "in,";
     cmd << rate << ",";
     cmd << server << ",";
     cmd << port << ",";
+    cmd << "udp" << ",";
     cmd << protocol;
     cmd << " --fdm=null";
     cmd << " --disable-ai-traffic";
@@ -39,15 +57,29 @@ void launch_fg(const std::string& aircraft, const std::string& rate, const std::
 
     // std::cout << cmd.str();
     system(cmd.str().c_str());
+    system("sleep 15");
 }
 
-struct Controls{
-    float aileron, elevator, rudder, throttle;
+#pragma pack(push, 1)
+struct GenericPacket{
+    double throttle;
+    double aileron;
+    double elevator;
+    double rudder;
+    double lat;
+    double lon; // long is reserved :)
+    double alt;
+    double roll;
+    double pitch;
+    double hdg;
+    double p;
+    double q;
+    double r;
+    double beta;
+    double alpha;
+    double v;
 };
-
-struct States{
-    float x, y;
-};
+#pragma pack(pop)
 
 
 int main()
@@ -56,26 +88,24 @@ int main()
     int port = 5000;
 
     // Launching FlightGear configured for UDP
-    launch_fg("f18", "60", addr, std::to_string(port), "udp");
-    system("sleep 10");
+    launch_fg("f18", "60", addr, std::to_string(port), "generic");
 
     UDPConnect *udp_connect;
     udp_connect = new UDPConnect((char *) addr.c_str(), port);
     
-    Controls ctrl;
+    GenericPacket pkt;
+    size_t pkt_len = sizeof(pkt);
 
-    ctrl.aileron  = 1.0;
-    ctrl.elevator = 0.0;
-    ctrl.rudder   = 0.0;
-    ctrl.throttle = 0.0;
+    for(int t = 0; t < 10; t++){
+        pkt.throttle = 0.0;
+        pkt.aileron  = 1.0;
+        pkt.elevator = 0.0;
+        pkt.rudder   = 0.0;
+        pkt.alt      = 100;
 
-    size_t msg_length = sizeof(ctrl);
-
-    for(int t = 0; t < 1000; t++){
-        udp_connect->send(&ctrl, msg_length);
+        udp_connect->send(&pkt, pkt_len);
         std::cout << t << '\n';
-        system("sleep 0.1");
+        system("sleep 0.01");
     }
-
     return 0;
 }
---
+++ Cpp-env/fg/main.h
@@ -0,0 +1,30 @@
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <ifaddrs.h>
+#include <net/if.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <iostream>
+#include <sstream>
+#include <cstdio>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <array>
+
+std::string exec(const char* cmd) {
+    std::array<char, 128> buffer;
+    std::string result;
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+    if (!pipe) {
+        throw std::runtime_error("popen() failed!");
+    }
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+        result += buffer.data();
+    }
+    return result;
+}
