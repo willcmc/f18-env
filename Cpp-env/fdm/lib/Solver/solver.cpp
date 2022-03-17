5b9eed267ac7a18c7104a8820d2b70689398351e
aman226 <amanmain3@gmail.com>
2022-03-17 10:05:57 +0530

    Update solver.cpp

--- Cpp-env/fdm/lib/Solver/solver.cpp
+++ Cpp-env/fdm/lib/Solver/solver.cpp
@@ -7,27 +7,28 @@
 
 void rk4(double* x, double* dx, float Ts, int t, double x_new[3][12], double* cntl,double* F18_Aerodata,double* ALPHA_BREAK, double* Geom)
 {
-   double dx_new[3][12];
-   float K1,K2,K3,K4;
+  double dx_new[3][12];
+  float K1,K2,K3,K4;
    
    
-   double DCG[3];
-   double FORCES[3];
-   double MOMENTS[3];
+  double DCG[3];
+  double FORCES[3];
+  double MOMENTS[3];
    
-   double T_atm;
-   double p_atm; 
-   double rho;
-   double M;
-   double g;
-   double Thrust[2];
+  double T_atm;
+  double p_atm; 
+  double rho;
+  double M;
+  double g;
+  double Thrust[2];
 
-   for (int i = 0; i <12; i++)
-   {
-      K1 = Ts*dx[i];
-      x_new[0][i] += K1/2;
-      x[i] += K1/6;
-   }
+  for (int i = 0; i <12; i++)
+  {
+    K1 = Ts*dx[i];
+    x_new[0][i] += K1/2;
+    x[i] += K1/6;
+  }
+  
   Atmosphere(x_new[0], &T_atm, &p_atm, &rho, &M, &g);
   Engine(t, Ts/2, x_new[0], cntl, M, g, Thrust); 
   Equations_of_Motion(x_new[0], g, ALPHA_BREAK, F18_Aerodata, Thrust, Geom, Geom, F18_Aerodata, g, cntl, dx_new[0], FORCES, MOMENTS, DCG);
